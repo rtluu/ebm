b53f90cfeec4f8f9a32468f99dc47339c795acb6
Ryan Luu <ryan@vumble.com>
2020-02-19 20:55:31 -0500

    Netlify Form

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,75 +1,69 @@
 import React, { useState } from "react"
-import axios from "axios";
+import { navigate } from 'gatsby-link'
 import "./form.scss";
+
+function encode(data) {
+    return Object.keys(data)
+        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+}
 
 
 const Form = () => {
 
-    const [serverState, setServerState] = useState({
-        submitting: false,
-        status: null
-    });
-    const handleServerResponse = (ok, msg, form) => {
-        setServerState({
-            submitting: false,
-            status: { ok, msg }
-        });
-        if (ok) {
-            form.reset();
-        }
-    };
-    const handleOnSubmit = e => {
-        e.preventDefault();
-        const form = e.target;
-        setServerState({ submitting: true });
-        axios({
-            method: "post",
-            url: "https://getform.io/f/c71599a0-dade-4f89-908f-0041de93b6dc",
-            data: new FormData(form)
+    const [state, setState] = React.useState({})
+
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({
+                'form-name': form.getAttribute('name'),
+                ...state,
+            }),
         })
-            .then(r => {
-                handleServerResponse(true, "Thanks!", form);
-            })
-            .catch(r => {
-                handleServerResponse(false, r.response.data.error, form);
-            });
-    };
+            .then(() => navigate(form.getAttribute('action')))
+            .catch((error) => alert(error))
+    }
+
     return (
         <div className="form-holder">
             <div className="form">
-                <form onSubmit={handleOnSubmit} autocomplete="off">
+                <form autoComplete="off" action="/weddings/" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
+                    <input type="hidden" name="form-name" value="contact" />
                     <div className="form-group">
-                        <label className="required" for="occasion">Occasion</label>
-                        <input type="occasion" name="occasion" className="form-control" id="occasion" aria-describedby="occasion" required="required" />
+                        <label className="required" >Occasion</label>
+                        <input type="text" name="occasion" className="form-control" id="occasion" aria-describedby="occasion" required="required" />
                     </div>
                     <div className="form-group">
-                        <label for="venue">Venue</label>
+                        <label >Venue</label>
                         <input type="text" name="venue" className="form-control" id="venue" />
                     </div>
                     <div className="form-group short">
-                        <label for="date">Event date</label>
+                        <label >Event date</label>
                         <input type="text" name="date" className="form-control" id="date" />
                     </div>
                     <div className="form-group">
-                        <label className="required" for="Name">Full name</label>
+                        <label className="required" >Full name</label>
                         <input type="text" name="Name" className="form-control" id="Name" required="required" />
                     </div>
                     <div className="form-group">
-                        <label className="required" for="email">Email address</label>
+                        <label className="required" >Email address</label>
                         <input type="email" name="email" className="form-control" id="email" aria-describedby="email" required="required" />
                     </div>
                     <div className="form-group short">
-                        <label for="phone">Phone number</label>
+                        <label >Phone number</label>
                         <input type="text" name="phone" className="form-control" id="phone" />
                     </div>
-                    <button type="submit" className="btn btn-primary" disabled={serverState.submitting}>
+                    <button type="submit" className="btn btn-primary">
                         Submit
                     </button>
-                    {serverState.status && (
-                        <p className={!serverState.status.ok ? "errorMsg" : ""}>
-                            {serverState.status.msg}
-                        </p>
-                    )}
                 </form>
             </div>
         </div>
