882f1b3b3ea6aba0ef43daf723df89404043bd2b
Ryan Luu <ryan@vumble.com>
2020-03-08 14:43:34 -0400

    Updated Favicon & Form

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -28,7 +28,7 @@ const Form = () => {
             data: new FormData(form)
         })
             .then(r => {
-                handleServerResponse(true, "Thanks!", form);
+                handleServerResponse(true, "Thanks for your inquiry, you'll hear from us shortly!", form);
             })
             .catch(r => {
                 handleServerResponse(false, r.response.data.error, form);
@@ -39,29 +39,30 @@ const Form = () => {
             <div className="form">
                 <form onSubmit={handleOnSubmit} autocomplete="off">
                     <div className="form-group">
-                        <label className="required" for="occasion">Occasion</label>
-                        <input type="occasion" name="occasion" className="form-control" id="occasion" aria-describedby="occasion" required="required" />
+                        <label className="required" for="Name">Your name</label>
+                        <input type="text" name="Name" className="form-control" id="Name" required="required" placeholder="Kelly Jane" />
                     </div>
                     <div className="form-group">
-                        <label for="venue">Venue</label>
-                        <input type="text" name="venue" className="form-control" id="venue" />
+                        <label className="required" for="email">Your email</label>
+                        <input type="email" name="email" className="form-control" id="email" aria-describedby="email" required="required" placeholder="kjane@gmail.com" />
                     </div>
                     <div className="form-group short">
-                        <label for="date">Event date</label>
-                        <input type="text" name="date" className="form-control" id="date" />
+                        <label for="phone">Your phone number</label>
+                        <input type="text" name="phone" className="form-control" id="phone" placeholder="202-555-2255" />
                     </div>
                     <div className="form-group">
-                        <label className="required" for="Name">Full name</label>
-                        <input type="text" name="Name" className="form-control" id="Name" required="required" />
+                        <label className="required" for="occasion">What type of event are you planning?</label>
+                        <input type="occasion" name="occasion" className="form-control" id="occasion" aria-describedby="occasion" required="required" placeholder="Wedding, party, etc." />
                     </div>
                     <div className="form-group">
-                        <label className="required" for="email">Email address</label>
-                        <input type="email" name="email" className="form-control" id="email" aria-describedby="email" required="required" />
+                        <label for="venue">Have you booked venue?</label>
+                        <input type="text" name="venue" className="form-control" id="venue" placeholder="'The W' or 'Not yet'" />
                     </div>
-                    <div className="form-group short">
-                        <label for="phone">Phone number</label>
-                        <input type="text" name="phone" className="form-control" id="phone" />
+                    <div className="form-group">
+                        <label for="date">Have you set a date?</label>
+                        <input type="text" name="date" className="form-control" id="date" placeholder="'May 12th' or 'Not yet'" />
                     </div>
+
                     <button type="submit" className="btn btn-primary" disabled={serverState.submitting}>
                         Submit
                     </button>
