b01c65b6f0eb6ef4bdaad2a1c55ebf162eb37434
Ryan Luu <ryan@vumble.com>
2020-03-20 11:26:07 -0400

    Media Query on Couples Page

--- src/components/Options/options.js
+++ src/components/Options/options.js
@@ -39,15 +39,15 @@ function Options() {
                 </div>
             </div>
             <div className="option">
-                <div className={"option-inner " + partialexpand + ""}>
+                <div className={"option-inner partial " + partialexpand + ""}>
+                    <div className="option-image-holder">
+                        <div className="option-image-background partial-planning" />
+                        <Partial />
+                    </div>
                     <div className="option-text">
                         <h2>Partial Planning</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <button onClick={partialClick}>LEARN MORE</button>
-                    </div>
-                    <div className="option-image-holder">
-                        <div className="option-image-background partial-planning" />
-                        <Partial />
                     </div>
                 </div>
             </div>
